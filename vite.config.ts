75cce217942abd8b6e8510460bcb7f05d1cb587e
HLN177 <harryleen177@gmail.com>
2024-12-17 00:25:40 +0000

    feat: auto import vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: [
+        'vue',
+        'vitest'
+        // {
+        //   '@testing-library/vue': [
+        //     'render',
+        //     'fireEvent',
+        //     ['screen', 'vtlScreen'],
+        //     'cleanup'
+        //   ]
+        // }
+      ],
+      dts: './auto-imports.d.ts'
+    })
+  ]
+});
