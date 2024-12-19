efc1e2631c175a9af0cae7c65766122649d03b54
HLN177 <harryleen177@gmail.com>
2024-12-19 16:06:18 +0000

    fix: type

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,11 +1,9 @@
 import type { App } from 'vue';
 import i18n from '@/shared/i18n/i18n';
-import { createPinia } from 'pinia';
+import pinia from '@/stores';
 import vuetify from './vuetify';
 
 export function registerPlugins(app: App) {
-  const pinia = createPinia();
-
   app.use(vuetify);
   app.use(pinia);
   app.use(i18n);
